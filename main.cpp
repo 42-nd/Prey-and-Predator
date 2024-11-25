eba1ed8a47272621fb73e3433d6c33cc6864bb1b
42-nd <60535433+42-nd@users.noreply.github.com>
2024-11-25 09:19:39 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -11,7 +11,36 @@ using namespace std;
 // u' = α * u - β * u * v
 // v' = δ * u * v - γ * v
 // Начальные условия: u(0) = 4212, v(0) = 552
-
+void update_parameters(int month, double& alpha, double& beta, double& gamma, double& delta) {
+    if (month >= 3 && month <= 4) {
+        // Весна: средний рост зайцев, умеренная активность лис
+        alpha = 0.3;
+        beta = 0.001;
+        gamma = 0.15;
+        delta = 0.00008;
+    } 
+    else if (month >= 5 && month <= 8) {
+        // Лето: высокий рост зайцев, высокая активность лис
+        alpha = 0.5;
+        beta = 0.0012;
+        gamma = 0.17;
+        delta = 0.0001;
+    }
+    else if (month >= 9 && month <= 10) {
+        // Осень: умеренный спад зайцев, активность лис снижается
+        alpha = 0.25;
+        beta = 0.0009;
+        gamma = 0.16;
+        delta = 0.00007;
+    }
+    else {
+        // Зима: сильное снижение численности зайцев, низкая активность лис
+        alpha = 0.15;
+        beta = 0.0005;
+        gamma = 0.2;
+        delta = 0.00005;
+    }
+}
 int main() {
     Cauchy_Problem<double> Problem;
 
@@ -38,14 +67,13 @@ int main() {
     };
 
 
-    double X0 = 0, Xn = 365;
-    int n = 365;
+    double X0 = 0, Xn = 365*3;
+    int n = 365*3;
     double h = (Xn - X0) / n;
 
     // Начальные условия
     vector<double> U0_erk = { 4212, 552 }; // Начальные условия: x0 = 4212, y0 = 552
     vector<double> U0_adams = { 4212, 552 };
-    vector<double> U0_moulton = { 4212, 552 };
 
     vector<vector<double>> adams_starter = Problem.RungeKutta_Start(0, h, 3, U0_adams, Source_Term);
     vector<vector<double>> combined_vector_adams;
@@ -53,7 +81,12 @@ int main() {
     combined_vector_adams.insert(combined_vector_adams.end(), adams_starter.begin(), adams_starter.end());
 
 
-    for (int i = 1; i <= n; i++) {
+    for (int i = 270; i <= n; i++) {
+        int month = ((i * (int)h) / 30) % 12 + 1; 
+        update_parameters(month, alpha, beta, gamma, delta);
+        cout << left << "\n\nCurrent month: " << month << endl;
+        cout << left << "Params:\n" << "Alpha: " << alpha << "\nBeta" << beta << endl;
+        cout << left << "Gamma: " << gamma << "\nDelta" << delta;
         Point P_Begin((i - 1) * h, 0, 0), P_End(i * h, 0, 0);
         // Решение задачи Коши по схеме Рунге-Кутты 4
         auto Result_ERK = Problem.ERKs_Scheme_Start(
--- plot.py
+++ plot.py
@@ -5,28 +5,43 @@ import matplotlib.pyplot as plt
 
 datafile = sys.argv[1]
 
+
 data = pd.read_csv(datafile)
+
+max_days = data['Time'].max()
+
+if max_days >= 3650/2: 
+    time_scale = 'Years'
+    data['Time_Scaled'] = data['Time'] / 365
+elif max_days >= 365: 
+    time_scale = 'Months'
+    data['Time_Scaled'] = data['Time'] / 30
+elif max_days >= 7:  
+    time_scale = 'Weeks'
+    data['Time_Scaled'] = data['Time'] / 7
+else:
+    time_scale = 'Days'
+    data['Time_Scaled'] = data['Time']
+
 
 plt.figure(figsize=(18, 6))
 
 
 plt.subplot(1, 3, 1)
-plt.plot(data['Time'], data['Prey'], label='Prey', color='blue')
-plt.xlabel('Days')
+plt.plot(data['Time_Scaled'], data['Prey'], label='Prey', color='blue')
+plt.xlabel(time_scale)
 plt.ylabel('Population')
 plt.title(f'{datafile} Prey Population Over Time')
 plt.legend()
 plt.grid(True)
 
-
 plt.subplot(1, 3, 2)
-plt.plot(data['Time'], data['Predator'], label='Predators', color='red')
-plt.xlabel('Days')
+plt.plot(data['Time_Scaled'], data['Predator'], label='Predators', color='red')
+plt.xlabel(time_scale)
 plt.ylabel('Population')
 plt.title(f'{datafile} Predator Population Over Time')
 plt.legend()
 plt.grid(True)
-
 
 plt.subplot(1, 3, 3)
 plt.scatter(data['Prey'], data['Predator'], label='Prey vs Predators', color='green')
@@ -36,6 +51,6 @@ plt.title(f'{datafile} Prey vs Predator Population')
 plt.legend()
 plt.grid(True)
 
-
+# Финальная настройка и отображение
 plt.tight_layout()
 plt.show()
